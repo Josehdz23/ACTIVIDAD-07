4b97ed2105d4053c43ed66c7dee1ba967bcccda5
Josehdz23 <jose.jahr.2016@gmail.com>
2025-07-17 01:13:11 -0600

    Fin del programa, todas las validaciones están hechas, desde los try hasta las validaciones de las notas y validaciones básicas que faltaban, se mejoró la función buscar y mostrar estudiantes, agregandole la suma total por curso, y mostrando el promedio total de los cursos para ambas funciones

--- Actividad 07.py
+++ Actividad 07.py
@@ -7,13 +7,17 @@ def registrar_Estudiantes():
         try:
             print("\n===== REGISTRAR ESTUDIANTES =====")
             cantidad = int(input("\n¿Cuántos estudiantes quiere registrar?: "))
-            for i in range(cantidad):
+            if cantidad > 0:
+                for i in range(cantidad):
                     try:
                         while True:
                             try:
                                 carnet = int(input(f"\nIngrese el carnet del estudiante {i + 1}: "))
                                 if carnet > 0:
-                                    break
+                                    if carnet in estudiantes:
+                                        print("\nEl estudiante ya existe, ingrese un nuevo carnet")
+                                    else:
+                                        break
                                 else:
                                     print("\nEl carnet no es valido, reintente")
                             except Exception as e:
@@ -43,71 +47,83 @@ def registrar_Estudiantes():
                             "Nombre": nombre,
                             "Edad": edad,
                             "Carrera": carrera,
-                            "Cursos":{}
+                            "Cursos": {}
                         }
                         while True:
                             try:
                                 cursos = int(input(f"Ingrese cuantos cursos se le agregaran al estudiante {i + 1}: "))
-                                for j in range(cursos):
-                                    while True:
-                                        try:
-                                            nombreCurso = input(f"\nIngrese el nombre del curso {j + 1}: ")
-                                            if nombreCurso or nombreCurso.isspace():
-                                                break
-                                            else:
-                                                print("\nEl nombre del curso no es valido, reintente")
-                                        except Exception as e:
-                                            print(f"\nEl nombre del curso no es valido, reintente {e}")
-                                    while True:
-                                        try:
-                                            notaTareas = float(
-                                                input(f"Ingrese la nota de las tareas del curso {j + 1}: "))
-                                            if notaTareas >= 0 and notaTareas <= 30:
-                                                break
-                                            else:
-                                                print("\nLa nota de las tareas no es valida, reintente")
-                                        except Exception as e:
-                                            print(f"\nLa nota de las tareas no es valida, reintente {e}")
-                                    while True:
-                                        try:
-                                            notaParcial = float(
-                                                input(f"Ingrese la nota del parcial del curso {j + 1}: "))
-                                            if notaParcial >= 0 and notaParcial <= 30:
-                                                break
-                                            else:
-                                                print("\nLa nota del parcial no es valida, reintente")
-                                        except Exception as e:
-                                            print(f"\nEl nota del parcial no es valida, reintente {e}")
-                                    while True:
-                                        try:
-                                            notaProyecto = float(
-                                                input(f"Ingrese la nota del proyecto del curso {j + 1}: "))
-                                            if notaProyecto >= 0 and notaProyecto <= 40:
-                                                break
-                                            else:
-                                                print("\nLa nota del proyecto no es valida, reintente")
-                                        except Exception as e:
-                                            print(f"\nLa nota del proyecto no es valida, reintente {e}")
-                                    estudiantes[carnet]["Cursos"][nombreCurso] = {
-                                        "Notas Tareas": notaTareas,
-                                        "Notas Parcial": notaParcial,
-                                        "Notas Proyecto": notaProyecto
-                                    }
-                                break
+                                if cursos > 0:
+                                    for j in range(cursos):
+                                        while True:
+                                            try:
+                                                nombreCurso = input(f"\nIngrese el nombre del curso {j + 1}: ")
+                                                if nombreCurso or nombreCurso.isspace():
+                                                    break
+                                                else:
+                                                    print("\nEl nombre del curso no es valido, reintente")
+                                            except Exception as e:
+                                                print(f"\nEl nombre del curso no es valido, reintente {e}")
+                                        while True:
+                                            try:
+                                                notaTareas = float(
+                                                    input(f"Ingrese la nota de las tareas del curso {j + 1}: "))
+                                                if notaTareas >= 0 and notaTareas <= 30:
+                                                    break
+                                                else:
+                                                    print("\nLa nota de las tareas no es valida, reintente")
+                                            except Exception as e:
+                                                print(f"\nLa nota de las tareas no es valida, reintente {e}")
+                                        while True:
+                                            try:
+                                                notaParcial = float(
+                                                    input(f"Ingrese la nota del parcial del curso {j + 1}: "))
+                                                if notaParcial >= 0 and notaParcial <= 30:
+                                                    break
+                                                else:
+                                                    print("\nLa nota del parcial no es valida, reintente")
+                                            except Exception as e:
+                                                print(f"\nEl nota del parcial no es valida, reintente {e}")
+                                        while True:
+                                            try:
+                                                notaProyecto = float(
+                                                    input(f"Ingrese la nota del proyecto del curso {j + 1}: "))
+                                                if notaProyecto >= 0 and notaProyecto <= 40:
+                                                    break
+                                                else:
+                                                    print("\nLa nota del proyecto no es valida, reintente")
+                                            except Exception as e:
+                                                print(f"\nLa nota del proyecto no es valida, reintente {e}")
+                                        estudiantes[carnet]["Cursos"][nombreCurso] = {
+                                            "Notas Tareas": notaTareas,
+                                            "Notas Parcial": notaParcial,
+                                            "Notas Proyecto": notaProyecto
+                                        }
+                                    break
+                                else:
+                                    print("\nEl numero de cursos no es valido, reintente")
                             except Exception as ex3:
                                 print(f"\nOcurrió un error: {ex3}, reintente")
                     except Exception as ex2:
                         print(f"\nOcurrió un error: {ex2}, reintente")
-            break
+                print("\nSe ha agregado el estudiante")
+                break
+            else:
+                print("\nDato invalido, reintente")
         except Exception as ex:
             print(f"\nOcurrió un error: {ex}, reintente")
 
 def mostrar_estudiantes():
     if estudiantes:
         for clave, datos in estudiantes.items():
+            sumaTotal = 0
+            noCursos = len(estudiantes[clave]["Cursos"])
             print(f"\nCarnet: {clave}, Nombre: {datos['Nombre']}, Edad: {datos['Edad']}, Carrera: {datos['Carrera']}\nCursos: ")
             for clave2, datos2 in datos["Cursos"].items():
                 print(f"Nombre del curso: {clave2}, Nota Tareas: {datos2["Notas Tareas"]}, Nota Parcial: {datos2["Notas Parcial"]}, Nota Proyecto: {datos2["Notas Proyecto"]}")
+                suma = datos2["Notas Tareas"] + datos2["Notas Parcial"] + datos2["Notas Proyecto"]
+                sumaTotal = sumaTotal + suma
+                print(f"Nota total del curso: {suma}")
+            print(f"El promedio de todos los cursos del estudiante con CARNET: {clave} es: {sumaTotal/noCursos}")
     else:
         print("\nNo hay estudiantes registrados! ")
 
@@ -119,9 +135,17 @@ def buscar_Estudiantes():
                 if buscar in estudiantes.keys():
                     for clave, datos in estudiantes.items():
                         if buscar == clave:
-                            print(f"\nCarnet: {clave}, Nombre: {datos['Nombre']}, Edad: {datos['Edad']}, Carrera: {datos['Carrera']}\nCursos: ")
+                            sumaTotal = 0
+                            noCursos = len(estudiantes[clave]["Cursos"])
+                            print(
+                                f"\nCarnet: {clave}, Nombre: {datos['Nombre']}, Edad: {datos['Edad']}, Carrera: {datos['Carrera']}\nCursos: ")
                             for clave2, datos2 in datos["Cursos"].items():
-                                print(f"Nombre del curso: {clave2}, Nota Tareas: {datos2["Notas Tareas"]}, Nota Parcial: {datos2["Notas Parcial"]}, Nota Proyecto: {datos2["Notas Proyecto"]}")
+                                print(
+                                    f"Nombre del curso: {clave2}, Nota Tareas: {datos2["Notas Tareas"]}, Nota Parcial: {datos2["Notas Parcial"]}, Nota Proyecto: {datos2["Notas Proyecto"]}")
+                                suma = datos2["Notas Tareas"] + datos2["Notas Parcial"] + datos2["Notas Proyecto"]
+                                sumaTotal = sumaTotal + suma
+                                print(f"Nota total del curso: {suma}")
+                            print(f"El promedio de todos los cursos del estudiante con CARNET: {clave} es: {sumaTotal / noCursos}")
                     break
                 else:
                     print("\nNo se encontró el estudiante! ")
